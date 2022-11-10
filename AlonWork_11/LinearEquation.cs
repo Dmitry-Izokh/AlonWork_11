8e3746984b3052384e8d88236b46ed0f11d7272d
Dmitry <Dmitry@DESKTOP-V8KGT4B>
2022-11-10 20:32:54 +0300

    Добавьте файлы проекта.

---
+++ AlonWork_11/LinearEquation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlonWork_11
+{
+    struct LinearEquation
+    {
+        double k;
+        double b;
+
+        public LinearEquation(double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+        public string Root()
+        {
+            double x;
+            if (k == 0 && b == 0)
+            {
+                return "Корень уравнения корень уравнения может принимать любое значение";
+            }
+            else if (k == 0 && b != 0)
+            {
+                return "Уравнение не имеет корней";
+            }
+            else if (k != 0 && b == 0)
+            {
+                return "Корень уравнения 0";
+            }
+            else if (k != 0 && b != 0)
+            {
+                x = -b / k;
+                return $"Корень уравнения {x}";
+            }   
+
+        }
+    }
+}
---
+++ AlonWork_11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlonWork_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение коэффициента k = ");
+            double k = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите значение коэффициента b = ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            
+
+            LinearEquation Linear = new LinearEquation(k, b);
+            Console.WriteLine(Linear.Root());
+            Console.ReadKey();
+
+        }
+    }
+}
